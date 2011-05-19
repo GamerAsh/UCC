bdc2a09b036247940ad4d86c9087353f977dfa16
Ashley Birtwell <ash@ubuntu.(none)>
2011-05-19 01:08:38 -0700

    back to a normal pre-friend state

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+UCC::Application.routes.draw do
+  resources :recover, :only =>[:new, :create]
+  resources :users   
+  resources :sessions,      :only => [:new, :create, :destroy]
+  resources :thoughts,    :only => [:create, :destroy]
+
+  root :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
+  match '/recover', :to => 'recover#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
+end
